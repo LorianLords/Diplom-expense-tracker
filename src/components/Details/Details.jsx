d6e4e9ef73e2b3eace0f8ba0e35d36ef150ca3e2
Kerim <frkeodal.rty@gmail.com>
2022-05-04 11:59:51 +0300

    Project has been done, without API

---
+++ src/components/Details/Details.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Card, CardHeader, CardContent, Typography} from "@material-ui/core";
+import {Doughnut} from 'react-chartjs-2'
+import useTransactions from "../../useTransactions";
+import useStyles from './styles';
+const Details = (props) => {
+    const classes = useStyles();
+    const {total, chartData} = useTransactions(props.title);
+    return(
+        <Card className={props.title === 'Income' ? classes.income : classes.expense}>
+            <CardHeader title={props.title} />
+            <CardContent>
+                <Typography variant={"h5"}>${total}</Typography>
+                <Doughnut data={chartData} />
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Details;
